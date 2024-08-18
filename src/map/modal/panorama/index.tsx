c35ebf3969eab7b1fc3de3aa4a44e41999d7f8b6
Yana Kukhta <kyv@b2bit.pro>
2024-08-19 00:29:39 +0300

    add reset to the panorama

--- src/map/modal/panorama/index.tsx
+++ src/map/modal/panorama/index.tsx
@@ -1,17 +1,41 @@
+import { useRef } from 'react'
+
 import ReactPannellum from 'react-pannellum'
 
+import styles from './styles.module.scss'
+
 export const Panorama = () => {
+  const viewerRef = useRef(null)
+
+  const handleSetPitch = () => {
+    if (viewerRef.current) {
+      ReactPannellum.setHfov(100)
+      ReactPannellum.setPitch(0)
+      ReactPannellum.setYaw(0)
+    }
+  }
+
   return (
-    <ReactPannellum
-      id="1"
-      sceneId="firstScene"
-      imageSource="/panorams/pano.jpg"
-      config={{
-        autoRotate: -2,
-        autoLoad: true,
-        title: 'Панорама',
-        description: 'Описание этой панорамы',
-      }}
-    />
+    <div>
+      <ReactPannellum
+        ref={viewerRef}
+        id="1"
+        sceneId="firstScene"
+        imageSource="/panorams/pano.jpg"
+        hfov={100}
+        pitch={0}
+        yaw={0}
+        config={{
+          autoRotate: true,
+          autoRotateInactivityDelay: 3000,
+          autoLoad: true,
+          title: 'Панорама',
+          description: 'Описание этой панорамы',
+        }}
+      />
+      <button className={styles.button} onClick={handleSetPitch}>
+        Сбросить панораму в исходную позицию
+      </button>
+    </div>
   )
 }
